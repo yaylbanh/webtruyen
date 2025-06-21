eaf208eb2f0b6aeea9ca8ae26ebc2e3bee6140c2
yaylbanh <ngochoa2603@gmail.com>
2025-06-21 21:23:16 +0700

    Commit cập nhật các file controller và giao diện quản lý

--- src/main/java/springboot/webtruyen/config/TomcatConfig.java
+++ src/main/java/springboot/webtruyen/config/TomcatConfig.java
@@ -1,9 +1,15 @@
 package springboot.webtruyen.config;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+
+import jakarta.servlet.MultipartConfigElement;
 
 @Configuration
 public class TomcatConfig {
@@ -12,9 +18,22 @@ public class TomcatConfig {
     public ServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
         factory.addConnectorCustomizers(connector -> {
-            connector.setMaxPostSize(209715200); // 200MB
-            connector.setMaxSavePostSize(209715200);
+            connector.setMaxPostSize(209_715_200); // 200MB
+            connector.setMaxSavePostSize(209_715_200);
         });
         return factory;
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+    
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(100));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(200));
+        return factory.createMultipartConfig();
     }
 }
