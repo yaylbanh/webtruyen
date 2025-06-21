05aca05b136b17a200212792c5c095b614d06d53
yaylbanh <ngochoa2603@gmail.com>
2025-06-21 21:39:52 +0700

    Commit cập nhật các file controller và giao diện quản lý

--- src/main/java/springboot/webtruyen/config/TomcatConfig.java
+++ src/main/java/springboot/webtruyen/config/TomcatConfig.java
@@ -1,13 +1,12 @@
 package springboot.webtruyen.config;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.unit.DataSize;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
 import jakarta.servlet.MultipartConfigElement;
 
@@ -19,20 +18,20 @@ public class TomcatConfig {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
         factory.addConnectorCustomizers(connector -> {
             connector.setMaxPostSize(209_715_200); // 200MB
-            connector.setMaxSavePostSize(209_715_200);
+            connector.setProperty("maxSwallowSize", "-1"); // allow very large post
         });
         return factory;
     }
 
     @Bean
-    public MultipartResolver multipartResolver() {
-        return new StandardServletMultipartResolver();
-    }
-    
-    @Bean
     public MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
-        factory.setMaxFileSize(DataSize.ofMegabytes(100));
+        factory.setMaxFileSize(DataSize.ofMegabytes(100));        // max per file
+        factory.setMaxRequestSize(DataSize.ofMegabytes(200));     // max total
+        factory.setFileSizeThreshold(DataSize.ofKilobytes(512));  // buffer size
+        factory.setLocation(System.getProperty("java.io.tmpdir"));
+
+        // 👇 Đây là dòng quan trọng cần thêm vào:
         factory.setMaxRequestSize(DataSize.ofMegabytes(200));
         return factory.createMultipartConfig();
     }
